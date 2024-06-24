4144bd32793ebe94c396c7510d10284c0dce8aa5
seong3664 <seong3664@gmail.com>
2024-06-24 14:51:36 +0900

    유니티로 만든 1인칭 슈팅 게임 연

---
+++ Survival_Island/Assets/02Scriptes/Hand_Ctrl.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Hand_Ctrl : MonoBehaviour
+{
+    public Animation ComBatSGAni;
+    public Light flashLight;
+    public AudioClip flashsound;   //소리파일
+    public AudioSource A_sourse;   //오디오 소스
+    void Start()    //게임 시작 전 호출
+    {
+        
+    }
+
+    void Update()    //게임 시작 후 계속 호출
+    {
+        GunCrtl();
+        flashlightonoff();
+    }
+
+    private void flashlightonoff()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            A_sourse.PlayOneShot(flashsound, 1.0f);
+            //소리 파일    소리 볼륨
+            flashLight.enabled = !flashLight.enabled;
+        }
+    }
+
+    private void GunCrtl()
+    {
+       
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+
+                ComBatSGAni.Play("running");
+        }
+        
+        else if (Input.GetKeyUp(KeyCode.LeftShift))    //왼쪽 쉬프트 키를 띄었다면
+        {
+            ComBatSGAni.Play("runStop");
+        }
+    }
+}
+
---
+++ Survival_Island/Assets/02Scriptes/Lightonoff.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_sensour : MonoBehaviour
+{
+    public Light StairLight;
+    public AudioSource source;
+    public AudioClip clip;
+    void Start()
+    {
+        
+    }
+    //is trrgger 했을 때 통과 하면서 충돌 감지 하는 함수
+    //콜백 함수라고 함 스스로 호출 하기 때문
+    private void OnTriggerEnter(Collider other)     //콜라이더는 충돌 관련구조체
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            source.PlayOneShot(clip,1.0f);
+            StairLight.enabled = true;
+            //충돌체의 태그를 감지한 후 맞으면 라이트 온
+        }
+    }
+    //콜라이더 범위에 들어왔다가 빠져나갔을 때
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            source.PlayOneShot(clip, 1.0f);
+            StairLight.enabled = false;
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
